2b820d8c9c83904e370f62dde0a12a1aa95ac840
Jessica Doss <doss.jessicam@gmail.com>
2019-07-05 09:46:10 -0700

    add UI

--- src/main.js
+++ src/main.js
@@ -3,3 +3,8 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+$(document).ready(function() {
+  $('.submit').click(function(){
+  })
+});
