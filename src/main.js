e1590972dd1ae19197d2c0e181c1edec3ce70a9e
Jessica Doss <doss.jessicam@gmail.com>
2019-07-05 10:10:37 -0700

    add code to main.js file

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-  $('.submit').click(function(){
+  $('.submit').click(function() {
+    let symptom = $('#symptoms').val();
+    $('#symptoms').val("");
+    $.ajax({
+      url: ''
+    })
   })
 });
