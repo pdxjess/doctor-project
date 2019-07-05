2824e65f4d46166fe37856fba6fb60da3e4ae735
Jessica Doss <doss.jessicam@gmail.com>
2019-07-05 11:39:59 -0700

    fixed doctor.js file after going back over course work

--- src/doctor.js
+++ src/doctor.js
@@ -1,22 +1,14 @@
 export class Doctor {
-}
-
-$(document).ready(function() {
-  $('.submit').click(function() {
-    let symptom = $('#symptoms').val();
-    $('#symptoms').val("");
-    $.ajax({
-      url: 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=d082b6969186b4d21a05df3d7a15f143'
-      ,
-      type:'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        $('').text('');
-        $('').text('');
-      },
+  doctorFinder(name) {
+    return new Promise(function(resolve){
+      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=d082b6969186b4d21a05df3d7a15f143';
+      request.onload = function() {
+        if(this.status === 200) {
+          resolve(request.response);
+        }
       }
-    })
-  })
-});
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
