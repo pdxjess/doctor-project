174040efba1c7d95818cf53ef3cb87ea94e10519
Student <example@example.com>
2019-07-15 12:39:11 -0700

    fix errors

--- src/doctor.js
+++ src/doctor.js
@@ -1,9 +1,9 @@
 export class Doctor {
   doctorFinder(doctor, condition) {
     return new Promise(function(resolve, reject){
-      const apiKey = process.env.apiKey;
+      // const apiKey = process.env.exports.apiKey;
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${docName}&query=${condition}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apikey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${doctor}&query=${condition}&location=or-portland&skip=0&limit=10&user_key=d082b6969186b4d21a05df3d7a15f143`;
       request.onload = function() {
         if(this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -5,20 +5,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-  $('#submit').submit(function(event) {
+  $('#form').submit(function(event) {
     event.preventDefault();
     let condition = $('#condition').val();
-    let docName = $('#docName').val();
+    let doctor = $('#name').val();
     $('#condition').val("");
-    $('#docName').val("");
+    $('#name').val("");
 
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-      $('#condition').val("");
-      $('#docName').val("");
-      $('#result').append('<li>' + element + '</li>');
-      }, function(error) {
-        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    });
-  });
-});
+    let betterDoctor = new Doctor();
+    let promise = betterDoctor.doctorFinder(condition, doctor);
+
+    promise.then(function (response) {
+         let body = JSON.parse(response);
+
+         if (body.data.length === 0) {
+           $('#result').append('There are no doctors matching this search, please try again');
+         }
+
+         for (let i = 0; i < body.data.length; i++) {
+
+           $('#result').append(`<strong>Name:</strong> ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}<br>
+           <strong>Address:</strong> ${body.data[i].practices[0].visit_address.street}<br>
+           <strong>Number:</strong> ${body.data[i].practices[0].phones[0].number}<br>
+           <strong>Website: </strong> ${body.data[i].practices[0].website} <br>
+           <strong>Accepting New Patients:</strong> ${body.data[i].practices[0].accepts_new_patients}<br> <br> <hr>`);
+         }
+       }, function (error) {
+
+         $('#result').text(`There was an error processing your request: ${error.message}`);
+       });
+     });
+
+   });
