dc93210a9d620ceb398b81e1f994462ecec61c5c
Jessica Doss <doss.jessicam@gmail.com>
2019-07-21 17:29:52 -0700

    show results fromdoctor api

--- src/main.js
+++ src/main.js
@@ -16,24 +16,23 @@ $(document).ready(function() {
     let promise = betterDoctor.doctorFinder(condition, doctor);
 
     promise.then(function (response) {
-         let body = JSON.parse(response);
+      let body = JSON.parse(response);
 
-         if (body.data.length === 0) {
-           $('#result').append('There are no doctors matching this search, please try again');
-         }
-
-         for (let i = 0; i < body.data.length; i++) {
-
-           $('#result').append(`<strong>Name:</strong> ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}<br>
-           <strong>Address:</strong> ${body.data[i].practices[0].visit_address.street}<br>
-           <strong>Number:</strong> ${body.data[i].practices[0].phones[0].number}<br>
-           <strong>Website: </strong> ${body.data[i].practices[0].website} <br>
-           <strong>Accepting New Patients:</strong> ${body.data[i].practices[0].accepts_new_patients}<br> <br> <hr>`);
-         }
-       }, function (error) {
-
-         $('#result').text(`There was an error processing your request: ${error.message}`);
+      if (body.data.length === 0) {
+        $('#result').append('There are no doctors for your search in this area.');
+        }
+      for (let i = 0; i < body.data.length; i++) {
+        $('#result').append(`<strong>Name:</strong> ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}<br>
+        <strong>Address:</strong> ${body.data[i].practices[0].visit_address.street}<br>
+        <strong>Number:</strong>
+        ${body.data[i].practices[0].phones[0].number}<br>
+        <strong>Website: </strong> ${body.data[i].practices[0].website} <br>
+        <strong>Email:</strong>
+        ${body.data[i].practices[0].email}<br>
+        <strong>Accepting New Patients:</strong> ${body.data[i].practices[0].accepts_new_patients}<br> <br> <hr>`);
+        }
+        }, function (error) {
+        $('#result').text(`There was an error processing your request: ${error.message}`);
        });
      });
-
    });
